16c3ce0c619f3bea0f9d27c32c4cc45757477ab4
Tyler Long <aethdae@gmail.com>
2026-02-18 16:46:43 -0600

    add eventListener

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const submitButton = document.getElementById("submitButton");
 const textArea = document.querySelector("textarea");
 
 // console.log(submitButton.innerHTML, textArea.placeholder);
+submitButton
+  .addEventListener("click", textToJson(textArea.textContent))
+  .preventDefault();
+
+function textToJson(string) {
+  try {
+    const json = JSON.parse(string);
+  } catch (err) {
+    console.error(`Error parsing text: ${err}`);
+  }
+}
