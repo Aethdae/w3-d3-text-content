e6256312b82db4e08193bef8538fe528dc59727f
Tyler Long <aethdae@gmail.com>
2026-02-19 15:53:36 -0600

    fix eventlistener, add function for JSON parsing, add function to create element

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const submitButton = document.getElementById("submitButton");
 const textArea = document.querySelector("textarea");
+const output = document.getElementById("outPut");
+const form = document.getElementById("jsonForm");
 
 // console.log(submitButton.innerHTML, textArea.placeholder);
-submitButton
-  .addEventListener("click", textToJson(textArea.textContent))
-  .preventDefault();
+jsonForm.addEventListener("submit", textToJson);
 
-function textToJson(string) {
+function textToJson(event) {
+  const string = textArea.value;
+  event.preventDefault();
+  console.log(`Logging: ${string}`);
   try {
     const json = JSON.parse(string);
+    createTextElement(json);
   } catch (err) {
+    output.textContent = `Error parsing text: ${err}`;
     console.error(`Error parsing text: ${err}`);
   }
+}
+
+function createTextElement(jsonObject) {
+  output.textContent = "";
+  const element = document.createElement("p");
+  const jsonString = JSON.stringify(jsonObject, null, 2);
+  element.textContent = jsonString;
+  output.appendChild(element);
 }
